ac19b473b599247a87e573e6870a50a72803e81b
NewLifeStyle101 <damian.myszko@gmail.com>
2026-07-01 11:56:28 +0100

    feat: initialization of the upload method error testing

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -68,5 +68,20 @@ mod test {
         Ok(())
     }
 
-    //async fn test_server_upload() {}
+    async fn test_server_upload() -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        type AnyResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+        let mut server = Listener::new("127.0.0.1", "0").await?;
+        let addr = server.local_addr()?;
+        let server_task: tokio::task::JoinHandle<AnyResult<()>> = tokio::spawn(async move {
+        let stream = server.accept_once().await?;
+            let mut conn = ProtocolConnection::new(stream).await?;
+
+            let len = conn.read_prefix().await?;
+            let body = conn.read_body(len).await?;
+            conn.send_header(&String::from_utf8_lossy(&body)).await?;
+
+            Ok(())
+        });
+        Ok(())
+    }
 }
