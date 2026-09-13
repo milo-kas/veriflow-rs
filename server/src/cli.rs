ab471d4c489dd983e84e2ba510f88b94b74eb0e2
NewLifeStyle101 <damian.myszko@gmail.com>
2026-09-13 01:41:46 +0100

    feat: adds cli config command

---
+++ server/src/cli.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Set configuration file values (ip, port, dir)
+    Config {
+        /// Set new ip / get ip if no value is provided
+        #[arg(short, long, num_args = 0..=1)]
+        ip: Option<Option<String>>,
+
+        /// Set new port / get port if no value is provided
+        #[arg(short, long, num_args = 0..=1)]
+        port: Option<Option<String>>,
+
+        /// Set new download directory / get dir if no value is provided
+        #[arg(short, long, num_args = 0..=1)]
+        dir: Option<Option<String>>,
+    },
+}
